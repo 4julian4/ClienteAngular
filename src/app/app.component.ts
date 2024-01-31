8e016f6d43743a1717dda3692164195722202e21
Andres Felipe <nbalamejor@hotmail.com>
2024-01-31 11:46:08 -0500

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalRService } from './signalr.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'ClienteAngular';
-}
+
+   @Component({
+     selector: 'app-root',
+     templateUrl: './app.component.html',
+     styleUrls: ['./app.component.css']
+   })
+
+   export class AppComponent implements OnInit {
+     mensaje: string = '';
+     mensajes: string[] = [];
+
+     constructor(private signalRService: SignalRService) {}
+
+     ngOnInit() {
+       this.signalRService.startConnection();
+       this.signalRService.mensajes$.subscribe((mensaje: string) => {
+         this.mensajes.push(mensaje);
+       });
+     }
+
+     enviarMensaje() {
+       if (this.mensaje.trim() !== '') {
+         this.signalRService.enviarMensaje(this.mensaje);
+         this.mensaje = '';
+       }
+     }
+   }
+
+
+   
---
+++ src/app/signalr.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { Observable, Subject } from 'rxjs';
+
+   @Injectable({
+     providedIn: 'root'
+   })
+   export class SignalRService {
+     private hubConnection: signalR.HubConnection;
+     private mensajeSubject = new Subject<string>();
+     mensajes$: Observable<string> = this.mensajeSubject.asObservable();
+
+     constructor() {
+       this.hubConnection = new signalR.HubConnectionBuilder()
+         .withUrl('https://localhost:7068/Myhub') // URL de tu servidor SignalR
+         .build();
+     }
+
+     startConnection() {
+       this.hubConnection
+         .start()
+         .then(() => this.hubConnection.on('ReceiveMessage', (user: string, mensaje: string) => {
+          this.mensajeSubject.next(mensaje);
+          console.log('Mensaje recibido: ' + mensaje);
+          console.log(this.hubConnection.connectionId);// aca  se obtiene el id de la conexion SR
+          //callback(mensaje);
+          
+        }))
+         .catch(err => console.log('Error al conectar con SignalR: ' + err));
+         
+         
+     }
+     
+     enviarMensaje(mensaje: string) {
+      
+       this.hubConnection.invoke('ObtenerPin',mensaje, '123')
+         .catch(err => console.error(err));
+       return this.hubConnection
+         .invoke('SendMessage',this.hubConnection.connectionId, mensaje)
+         .catch(err => console.error(err));
+     }
+
+     recibirMensaje(callback: (mensaje: string) => void) {
+       this.hubConnection.on('ReceiveMessage', (mensaje: string) => {
+         this.mensajeSubject.next(mensaje);
+         console.log('Mensaje recibido: ' + mensaje);
+         callback(mensaje);
+         
+       });
+     }
+
+     
+   }
+
+
+   
