f13ff8f73d9ba9a34a322ee41bfc62ddf9b3fd76
Andres Felipe <nbalamejor@gmail.com>
2024-05-24 14:01:55 -0500

    Ajuste de redirect

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,13 +9,13 @@ export const environment = {
     NombreAplicacion :"Rydent Web Nube",
     OAuth: {
         AuthCodeEndPoint : "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-        RedirectURI : "https://localhost:4200/auth/login-callback",
+        RedirectURI : "https://rydentclient.azurewebsites.net/auth/login-callback",
         ClientId : "2c9695c3-e82a-4112-a6bf-dd4beea8d27d",
         Scope : "https://graph.microsoft.com/User.Read",
         state : "1234567890",
     },
     OAuthGoogle: {
         ClientId : "761745688293-jra72nltsoor2g7enfjsu79te8pnnsdq.apps.googleusercontent.com",
-        RedirectURI : "https://localhost:4200/auth/login-callback-google",
+        RedirectURI : "https://rydentclient.azurewebsites.net/auth/login-callback-google",
     },
 };
