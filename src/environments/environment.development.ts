78cd5df142df597b2d97c6e21a4df355d3917e06
4julian4 <124397370+4julian4@users.noreply.github.com>
2024-06-05 23:16:06 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,20 +3,20 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
     production: false,
     httpOptions : { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) },
-    apiUrl : "https://rydentwebdv.azurewebsites.net/api",
+    apiUrl : "/api",
     signalRUrlJp : "https://localhost:63363/Rydenthub",
     signalRUrlLocal : "https://localhost:7028/Rydenthub",
     signalRUrl : "https://rydentwebdv.azurewebsites.net/Rydenthub",
     NombreAplicacion :"Rydent Web Nube(Desarrollo)",
     OAuth: {
         AuthCodeEndPoint : "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-        RedirectURI : "https://localhost:4200/auth/login-callback",
+        RedirectURI : "https://rydentclient.azurewebsites.net/auth/login-callback",
         ClientId : "2c9695c3-e82a-4112-a6bf-dd4beea8d27d",
         Scope : "https://graph.microsoft.com/User.Read",
         state : "1234567890",
     },
     OAuthGoogle: {
         ClientId : "761745688293-jra72nltsoor2g7enfjsu79te8pnnsdq.apps.googleusercontent.com",
-        RedirectURI : "https://localhost:4200/auth/login-callback-google",
+        RedirectURI : "https://rydentclient.azurewebsites.net/auth/login-callback-google",
     },
 };
